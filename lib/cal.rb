f1ec3258308b3b1f6733ee6e07edc4be3ec18ba4
Hassan Shamim <hassan.has.you@gmail.com>
2012-09-25 13:13:07 -0500

    Added year to intialization and removed week test

--- lib/cal.rb
+++ lib/cal.rb
@@ -2,41 +2,31 @@ YEAR = ARGV[1]
 MONTH = ARGV[0]
 
 class Week
-  attr_accessor :days, :sun, :mon, :tue, :wed, :thur, :fri, :sat
-
-  def initialize 
-    @days = [nil, nil, nil, nil, nil, nil, nil]
-    @sat = days[0]
-    @sun = days[1]
-    @mon = days[2]
-    @tues =days[3]
-    @wed = days[4]
-    @thur =days[5]
-    @fri = days[6]
-   
-  end 
-
 end
 
 
 class Month
 
-  def initialize( )
+  attr_reader :leap_year
 
-   @leap_year 
+  def initialize(year )
+
+    set_leap_year(year)
  
   end
 
-  def is_leap_year?(year = YEAR)
+  def set_leap_year(year = YEAR)
     if year % 400 == 0
-      true
+      @leap_year = true
     elsif year % 100 == 0
-      false
+      @leap_year = false
     elsif year % 4 == 0
-      true
+      @leap_year = true
     else
-      false
+      @leap_year = false
     end
   end
+
+
 
 end
