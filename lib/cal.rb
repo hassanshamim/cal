52df01e0d37e8191856d200be3215989b558615c
Hassan Shamim <hassan.has.you@gmail.com>
2012-09-25 14:16:44 -0500

    added calc_num_days function and tests

--- lib/cal.rb
+++ lib/cal.rb
@@ -7,11 +7,13 @@ end
 
 class Month
 
-  attr_reader :leap_year
+  attr_reader :leap_year, :num_days
 
-  def initialize(year )
-
+  def initialize( month, year )
+    @year = year
+    @month = month
     set_leap_year(year)
+    calc_num_days( month )
  
   end
 
@@ -27,6 +29,13 @@ class Month
     end
   end
 
+  def calc_num_days(month)
+    months_with_31_days = [1, 3, 5, 7, 8, 10, 12]
+    feb = 2
+
+    @days =  days_with_31_months.include?(month) ? 31 : 30
+    if month == feb then @days = ( @leap_year ? 29 : 28 ) end
+  end
 
 
 end
--- test/test_cal.rb
+++ test/test_cal.rb
@@ -9,7 +9,7 @@ class CalTest < Test::Unit::TestCase
 
   def test_02_set_leap_year_calcs_leap_years_and_works_with_defaults
 
-    m = Month.new( 0 )
+    m = Month.new( 0, 0 )
 
     assert_equal true, m.set_leap_year( 2000 )
     assert_equal false, m.set_leap_year( 1900 )
@@ -20,7 +20,23 @@ class CalTest < Test::Unit::TestCase
 
   def test_03_leap_year_returns_boolean
 
-  m = Month.new( 1337 )
+    m = Month.new( 1, 1337 )
+    assert_equal false, m.leap_year
+
+    m.set_leap_year( 60 )
+    assert_equal true, m.leap_year
+  end
+
+  def test_04_calc_num_days_returns_correct_number
+  
+    m1 = Month.new(1, 2001)
+    m2 = Month.new(9, 2001)
+    m3 = Month.new(2, 2001)
+    m4 = Month.new(2, 1996)
+    assert_equal  31, m1.num_days
+    assert_equal  30, m2.num_days
+    assert_equal  28, m3.num_days
+    assert_equal  29, m4.num_days
   end
 
 end
